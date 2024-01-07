f60c681de4555475d5aeafff85b535bedd184125
krishgokul2k <151126086+krishgokul2k@users.noreply.github.com>
2024-01-07 11:39:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-// const num = document.querySelector(".num");
-
-function addTask(){
+const num = document.querySelector("span");
+const link = document.querySelector("a");
+const get = document.querySelector(".get");
+console.log(get);
+function Task(){
+    let line =document.querySelector("hr");
+    if(inputBox.value === ''){
+        num.style.display = "none";
+        link.style.display = "none";
+        line.style.display = "none";
+    }
+    else{
+        num.style.display = "block";
+        link.style.display = "block";
+        line.style.display = "block";
+    }
+}
+const btn = document.querySelector("button");
+console.log(btn);
+btn.addEventListener("click", () =>{
     if(inputBox.value === ''){
         alert("you must write something!");
-    }
+    } 
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        // let span = document.createElement("span");
-        // span.innerHTML = "\u274C"
-        // li.appendChild(span);
-        let a = document.createElement("a");
-        a.innerHTML = inputBox.value;
+        let list = document.querySelectorAll("li");
+        console.log(list);
+        let arr = [list.length];
+        arr.map((index)=>{
+            get.innerHTML = index;
+        })
     }
-    inputBox.value = "";
-}
+    inputBox.value = '';
+})
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
@@ -26,6 +44,3 @@ listContainer.addEventListener("click", function(e){
         e.target.parentElement.remove();
     }
 }, false);
-
-
-
